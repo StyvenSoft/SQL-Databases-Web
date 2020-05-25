d4a2ec61256e00d5738ad53b52cc93e3b864f2d4
Steveen <steveen.echeverri@gmail.com>
2020-05-24 23:40:07 -0500

    Create group by

---
+++ 4-Aggregate Functions/6-group-by.sql
@@ -0,0 +1,51 @@
+--  Podríamos calcular cada número mediante una serie de consultas con diferentes WHERE afirmaciones
+
+SELECT AVG(imdb_rating)
+FROM movies
+WHERE year = 1999;
+
+SELECT AVG(imdb_rating)
+FROM movies
+WHERE year = 2000;
+
+SELECT AVG(imdb_rating)
+FROM movies
+WHERE year = 2001;
+
+-- Pero podemos GROUP BY hacer esto en un solo paso:
+
+SELECT year,
+   AVG(imdb_rating)
+FROM movies
+GROUP BY year
+ORDER BY year
+
+--  nuestra función agregada es COUNT()price
+
+SELECT price, COUNT(*) 
+FROM fake_apps
+GROUP BY price;
+
+-- agregue una WHEREcláusula para contar el número total de aplicaciones que se han descargado más de 20,000 veces, a cada precio.
+
+SELECT price, COUNT(*) 
+FROM fake_apps
+WHERE downloads > 20000
+GROUP BY price;
+
+-- calcule el número total de descargas para cada categoría.
+
+SELECT category, SUM(downloads)
+FROM fake_apps
+GROUP BY category;
+
+-- A veces, queremos GROUP BYun cálculo realizado en una columna.
+
+-- cuántas películas tienen calificaciones de IMDb que se redondean a 1, 2, 3, 4, 5. 
+
+SELECT ROUND(imdb_rating),
+   COUNT(name)
+FROM movies
+GROUP BY ROUND(imdb_rating)
+ORDER BY ROUND(imdb_rating);
+
