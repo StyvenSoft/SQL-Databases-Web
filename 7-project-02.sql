a5658ad30eaafb88d9b1d16cfd30849005b46ab6
Steveen <steveen.echeverri@gmail.com>
2020-05-27 12:31:16 -0500

    SELECT category, COUNT(*)

--- 7-project-02.sql
+++ 7-project-02.sql
@@ -47,3 +47,46 @@ WHERE stage = 'Seed';
 SELECT MIN(founded)
 FROM startups;
 
+-- Devuelve el promedio valuation.
+
+SELECT AVG(valuation)
+FROM startups;
+
+-- Devuelve el promedio valuation, en cada uno category
+
+SELECT category, AVG(valuation)
+FROM startups
+GROUP BY category;
+
+-- Redondea los promedios a dos decimales.
+
+SELECT category, ROUND(AVG(valuation), 2)
+FROM startups
+GROUP BY category;
+
+-- Primero, devuelva el nombre de cada uno categorycon el número total de compañías que le pertenecen.
+
+SELECT category, COUNT(*)
+FROM startups
+GROUP BY category;
+
+-- filtre el resultado para incluir solo categorías que tengan más de tres empresas.
+
+SELECT category, COUNT(*)
+FROM startups
+GROUP BY category
+HAVING COUNT(*) > 3
+ORDER BY 2 DESC;
+
+-- ¿Cuál es el tamaño promedio de una startup en cada uno location?
+
+SELECT location, AVG(employees)
+FROM startups
+GROUP BY location;
+
+-- ¿Cuál es el tamaño promedio de una startup en cada uno location, con tamaños promedio superiores a 500?
+
+SELECT location, AVG(employees)
+FROM startups
+GROUP BY location
+HAVING AVG(employees) > 500;
